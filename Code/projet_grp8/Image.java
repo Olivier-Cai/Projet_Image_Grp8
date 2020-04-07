4004eb49ba5bd434c2e924e274cbe46866dfd4b1
wiwiYY <willy.yu.wen@gmail.com>
2020-04-07 15:06:03 +0200

    maj des nouvelles methodes

--- Code/projet_grp8/Image.java
+++ Code/projet_grp8/Image.java
@@ -13,13 +13,15 @@ private static BufferedImage bfi;
 	}
 	
 	/**
+	 * boolean pour le depassement d'un seuil sur l'histogramme
+	 * si la ligne est deja depasser alors il ne prend pas en compte
 	 * 
 	 * @param depassed
 	 * @param seuilactuel
 	 * @param seuil
 	 * @return
 	 */
-	public static boolean descend(boolean depassed, int seuilactuel, int seuil) {
+	public static boolean isDepassed(boolean depassed, int seuilactuel, int seuil) {
 		if(!depassed) {
 			if(seuilactuel >= seuil) {
 				return true;
@@ -31,10 +33,10 @@ private static BufferedImage bfi;
 	}
 	
 	/**
-	 * trace le graphique des pixels noir en fonction de l'image sur l'axe X et Y et affiche le graph
+	 * trace le graphique des pixels noir en fonction de l'image sur l'axe X et affiche le graph
 	 * @throws IOException
 	 */
-	public static void HistoX () throws IOException {		
+	public static void histoX () throws IOException {		
 		BufferedImage imgBinaire = new BufferedImage(bfi.getWidth(), bfi.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
 	    Graphics2D surfaceImg = imgBinaire.createGraphics();
 	    surfaceImg.drawImage(bfi, null, null);      
@@ -46,7 +48,7 @@ private static BufferedImage bfi;
 	    int[]tabWidth=new int[width];
 	    int sum = 0;
 	    
-	    for (int y=0; y<height; y++) { //histograme par ligne
+	    for (int y=0; y<height; y++) { //histograme par ligne : axe X
 		    for (int x=0; x<width; x++) {
 		    	if(((bfi.getRGB(x, y)>>8)&0xff) == 0)
 		    		sum++;
@@ -55,40 +57,63 @@ private static BufferedImage bfi;
 		    sum=0;
 	    }   
 
-	    for (int x=0; x<width; x++) { //histograme par colonne
+	    //trace de l'histogramme ligne
+	    BufferedImage bi2 = new BufferedImage( width+50, height+50, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g2 = bi2.createGraphics();
+		for (int i=0; i<tabHeight.length; i++)
+			g2.drawLine(20, i, 20+(tabHeight[i]), i); 
+		
+		/**
+		 * compteur de ligne horizontale sur l'axe X 
+		 * Attention il faut prendre en compte le décalage de 
+		 * drawLine(20,..,20,..) dans les methodes histoX() et histoY()
+		 */
+		int seuilMarche=60; //a regler en fonction de l'image binaire
+	    int nbLigne=0;
+	    boolean depassed = false;
+	    for(int i=0; i<tabHeight.length; i++){
+	    	if(	isDepassed( depassed,  tabHeight[i], seuilMarche) ) { //voir method en haut
+	    		nbLigne++;
+	    	}
+	    }
+	    System.out.println("il peu y avoir "+nbLigne+" ligne(s)");   
+
+		Imshow.imshow(bi2);
+	    Imshow.imshow(bfi);
+	}
+	
+	/**
+	 * trace le graphique des pixels noir en fonction de l'image sur l'axe Y et affiche le graph
+	 * @throws IOException
+	 */
+	public static void histoY () throws IOException {		
+		BufferedImage imgBinaire = new BufferedImage(bfi.getWidth(), bfi.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+	    Graphics2D surfaceImg = imgBinaire.createGraphics();
+	    surfaceImg.drawImage(bfi, null, null);      
+	    bfi = imgBinaire;
+	    
+	    int width = bfi.getWidth();
+	    int height = bfi.getHeight();
+	    int[]tabHeight=new int[height];
+	    int[]tabWidth=new int[width];
+	    int sum = 0;
+
+	    for (int x=0; x<width; x++) { //histograme par colonne : axe Y
 	    	for (int y=0; y<height; y++) {
 	    		if(((bfi.getRGB(x, y)>>8)&0xff) == 0)
 	    			sum++;
 	    	}
 	    	tabWidth[x]=sum;
 	    	sum=0;
-	    }   
-	    //trace de l'histogramme ligne
-	    BufferedImage bi2 = new BufferedImage( width+100, height+500, BufferedImage.TYPE_INT_RGB);
-		Graphics2D g2 = bi2.createGraphics();
-		for (int i=0; i<tabHeight.length; i++)
-			g2.drawLine(20, i, 20+(tabHeight[i]), i); 
+	    }
 		
 		//trace de l'histogramme colonne
-		BufferedImage bi3 = new BufferedImage( width+100, height+500, BufferedImage.TYPE_INT_RGB);
+		BufferedImage bi3 = new BufferedImage( width+50, height+50, BufferedImage.TYPE_INT_RGB);
 		Graphics2D g3 = bi3.createGraphics();
 		//ligne droite
 		for (int i=0; i<tabWidth.length; i++)
 			g3.drawLine(i, 20, i, 20+(tabWidth[i]));
 		
-		
-		//compteur de ligne
-		int seuilMarche=60; //a regler en fonction de l'image binaire
-	    int nbLigne=0;
-	    boolean depassed = false;
-	    for(int i=0; i<tabHeight.length; i++){
-	    	if(	descend( depassed,  tabHeight[i], seuilMarche) ) { //voir method en haut
-	    		nbLigne++;
-	    	}
-	    }
-	    System.out.println("il peu y avoir "+nbLigne+" ligne(s)");   
-	    //TODO: probleme d'import , ce projet ne fonctionne pas comme d'habitude
-		Imshow.imshow(bi2);
 		Imshow.imshow(bi3);
 	    Imshow.imshow(bfi);
 	}
--- Code/projet_grp8/Main.java
+++ Code/projet_grp8/Main.java
@@ -23,74 +23,6 @@ public class Main {
 	      frame.pack(); 
 	      frame.setVisible(true);
 	 }
-	public static boolean descend(boolean depassed, int seuilactuel, int seuil) {
-		if(!depassed) {
-			if(seuilactuel >= seuil) {
-				return true;
-			}else
-				return false;
-		}
-		else
-			return false;
-	}
-	
-	public static void histoXY (BufferedImage img) throws IOException {		
-		BufferedImage imgBinaire = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
-	    Graphics2D surfaceImg = imgBinaire.createGraphics();
-	    surfaceImg.drawImage(img, null, null);      
-	    img = imgBinaire;
-	    
-	    int width = img.getWidth();
-	    int height = img.getHeight();
-	    int[]tabHeight=new int[height];
-	    int[]tabWidth=new int[width];
-	    int sum = 0;
-	    
-	    for (int y=0; y<height; y++) { //histograme par ligne
-		    for (int x=0; x<width; x++) {
-		    	if(((img.getRGB(x, y)>>8)&0xff) == 0)
-		    		sum++;
-		    }
-		    tabHeight[y]=sum;
-		    sum=0;
-	    }   
-
-	    for (int x=0; x<width; x++) { //histograme par colonne
-	    	for (int y=0; y<height; y++) {
-	    		if(((img.getRGB(x, y)>>8)&0xff) == 0)
-	    			sum++;
-	    	}
-	    	tabWidth[x]=sum;
-	    	sum=0;
-	    }   
-	    //trace de l'histogramme ligne
-	    BufferedImage bi2 = new BufferedImage( width+100, height+500, BufferedImage.TYPE_INT_RGB);
-		Graphics2D g2 = bi2.createGraphics();
-		for (int i=0; i<tabHeight.length; i++)
-			g2.drawLine(20, i, 20+(tabHeight[i]), i); 
-		
-		//trace de l'histogramme colonne
-		BufferedImage bi3 = new BufferedImage( width+100, height+500, BufferedImage.TYPE_INT_RGB);
-		Graphics2D g3 = bi3.createGraphics();
-		//ligne droite
-		for (int i=0; i<tabWidth.length; i++)
-			g3.drawLine(i, 20, i, 20+(tabWidth[i]));
-		
-		
-		//compteur de ligne
-		int seuilMarche=60; //a regler en fonction de l'image binaire
-	    int nbLigne=0;
-	    boolean depassed = false;
-	    for(int i=0; i<tabHeight.length; i++){
-	    	if(	descend( depassed,  tabHeight[i], seuilMarche) ) { //voir method en haut
-	    		nbLigne++;
-	    	}
-	    }
-	    System.out.println("il peu y avoir "+nbLigne+" ligne(s)");   
-		imshow(bi2);
-		imshow(bi3);
-	    imshow(img);
-	}
 	
 	public static void main (String[] args) throws IOException  {
 		//TODO: input image
@@ -129,6 +61,8 @@ public class Main {
 		//TODO: supression de bruit, amelioration de la qualite des marche 
 		// de l'escalier pour faciliter la reconnaissante du seuil 
 		
+		//filtre median de couleur
+		MedianFilter.median(path); 
 		
 
 		
@@ -176,8 +110,8 @@ public class Main {
         
 		//TODO:histogram par ligne et par colonne
         Image ima = new Image(imgSeuil);
-        ima.HistoX();
-//		histoXY(imgSeuil);
+        ima.histoX();
+        ima.histoY();
 		
 		//TODO; a ce stade il faut que les "marches" de l'escalier seulement soit en noir
 		
