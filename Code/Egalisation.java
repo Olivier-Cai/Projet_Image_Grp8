07df12e3d55c45176b310e88b1a5f890884d7983
Leoppa <47949401+Leoppa@users.noreply.github.com>
2020-04-22 20:36:58 +0200

    Version Beta
    A tester avec de meilleures photos et amélioration du code possible avec erosion ou dilatation

---
+++ Code/Egalisation.java
@@ -0,0 +1,88 @@
+package Image;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Egalisation {
+	/*
+	 * Affichage de l'image
+	 */
+	public static void imshow(BufferedImage image) throws IOException {
+	      //Instantiate JFrame 
+	      JFrame frame = new JFrame(); 
+
+	      //Set Content to the JFrame 
+	      frame.getContentPane().add(new JLabel(new ImageIcon(image))); 
+	      frame.pack(); 
+	      frame.setVisible(true);
+	}
+	
+	/*
+	 * Ecriture des barres de l'histogramme
+	 */
+	static BufferedImage zeros(int height, int width) {
+		int[] noir = {0,0,0,255};
+		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB); 
+		//WritableRaster raster = img.getRaster();
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height ; y++) {
+				img.getRaster().setPixel(x, y, noir);
+			}
+		}
+		return img; 
+	}
+	
+	/*
+	 * Egalisation de l'image du fichier f
+	 * @param f 
+	 */
+	public static void egalisation(BufferedImage img) {
+		int[] tab = new int[256];
+		int min = 256; 
+		int max = 0;
+		int[] tabCum = new int[256];
+		int cpt = 0;
+		int sum = 0; 
+		
+		for (int x = 0; x < img.getWidth(); x++) {
+			for (int y = 0; y < img.getHeight(); y++) {
+				int p = img.getRGB(x,y);
+				int r = (p>>16)&0xff;
+				tab[r]++;
+			
+				if(r<min) {
+					min = r; 
+				}
+				if (r>max) {
+					max = r;
+				}
+			}
+		}
+		
+		for (int x = 0; x < 256; x++) {
+			sum += tab[x]; 
+			tabCum[cpt] = sum;
+			cpt++;
+		}
+		
+		System.out.println("min "+min+", max "+max);
+		
+		int couleur[] = new int[3];
+		for (int x = 0; x < img.getWidth(); x++) {
+			for (int y = 0; y < img.getHeight(); y++) {
+				int p = img.getRGB(x,y);
+				int r = (p>>16)&0xff;
+				
+				int j = (((256*(tabCum[r]-1))/(img.getHeight()*img.getWidth())));
+				couleur[0] = j;
+				couleur[1] = j;
+				couleur[2] = j;
+				
+				img.getRaster().setPixel(x, y, couleur);
+			}
+		}
+	}
+}
---
+++ Code/Main.java
@@ -0,0 +1,49 @@
+package Image;
+
+import Image.ImageTest;
+import Image.Histogramme;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import Image.Sobel;
+import projet_grp8.Connexite;
+import projet_grp8.Imshow;
+
+public class Main {
+
+    public static void main (String [] args) {
+
+        try {
+            BufferedImage bf = ImageTest.chargerImage("/Users/leosok/Downloads/IMG_3530.JPG");
+            
+            
+           //ImageTest.seuillage(bf, 192);
+           
+           //ImageTest.egalisation(bf);
+            
+           ImageTest.median(bf);
+           ImageTest.imageBin(bf);
+           ImageTest.sobel(bf);
+           
+           
+           //ImageTest.connexite(bf);
+           //ImageTest.toNegatif(bf);
+        
+       
+   		int nbColora = Label8.getNumberOfCC(bf);
+   		//System.err.println("taille de haut en bas :"+bf.getHeight());
+   		Imshow.imshow(bf);
+   		System.out.println("nombre de marche avec connexite: "+((nbColora/2)+1));
+            
+        ImageTest.afficherImage(bf , "Coucou");
+        
+
+            // On calcul l'histogramme ici
+          //int [] his = Histogramme.normalise(Histogramme.calculHistogramme(bf)); // On calcul et normalise l'histogramme
+
+          // ImageTest.afficherHistogramme(his);
+
+        }catch (IOException e) {
+            System.out.println("ERREUR CHARGEMENT IMAGE.");
+        }
+    }
+}
