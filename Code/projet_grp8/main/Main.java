c28080c2c7bf3c60c22754bb2848ca31c39c96cf
wiwiYY <willy.yu.wen@gmail.com>
2020-05-09 00:57:20 +0200

    migrassion des combinaison de test1 dans le main et suppression du package tests

--- Code/projet_grp8/main/Main.java
+++ Code/projet_grp8/main/Main.java
@@ -17,9 +17,12 @@ import projet_grp8.util.*;
 public class Main {
 	
 	public static void main (String[] args) throws IOException  {
-		//TODO: input image
-		File file = new File("Bdd" + File.separator+"escalier212.jpg"); 
-		File file1 = new File("Bdd" + File.separator+"EscalierLeo.jpg"); 
+
+		File file = new File("Bdd" + File.separator+"escalier212.jpg"); //escalier212.jpg, marche couleur sombre
+		File file1 = new File("Bdd" + File.separator+"8.jpg"); //8.jpg, marche pas car trop de contraste B&N
+		File file2 = new File("Bdd" + File.separator+"escalierPerso.jpg"); //escalierPerso.jpg, marche de couleur sombre
+		File file3 = new File("Bdd" + File.separator+"imag5.jpg"); //imag5.jpg, marche pas car la marche est de couleur clair
+		File file4 = new File("Bdd" + File.separator+"eescalier.jpg"); //eescalier.jpg, marche pas car l'ombre a cote est colorie
 		BufferedImage img = null;
 		
 		// creation de class pour utiliser les methodes disponible
@@ -28,62 +31,63 @@ public class Main {
 		Otsu o = new Otsu();
 		Sobel sb = new Sobel();
 		ErosionDilatation ed = new ErosionDilatation();
-
+		Egalisation eg = new Egalisation();
+		
+		
 		try {
-			img = ImageIO.read(file);
+			img = ImageIO.read(file2);
+			Imshow.imshow(img);
 			//Imshow.imshow(img);
 		} catch (IOException e1) {
-			System.out.println("Erreur de lecture de fichier");
+			System.err.println("Erreur de lecture de fichier");
 		}		
-		Imshow.imshow(img);
+//		Imshow.imshow(img);
+		
 		//test 1.0 : gris > otsu > seuil > median
 		BufferedImage img1 = ic.gris(img);
-		img1 = mf.median(img1);
-		//utile pour l'image sans bruit
+//		Imshow.imshow(img1);
+		BufferedImage bj = mf.median(img1);
+//		Imshow.imshow(bj);
 		float seuil = o.otsu(img);
 		System.out.println("otsu seuil : "+seuil);
-		img1 = ic.seuillage(img, seuil); //image en NB avec un seuil auto grace a otsu
-		BufferedImage bj = mf.median(img1);  //supprime les bruits avec median 3
-		Imshow.imshow(bj);
+		float seuila = seuil-25;
+		img1 = ic.seuillage(img, seuila); //image en NB avec un seuil auto grace a otsu
+		bj = mf.median(img1);  //supprime les bruits avec median 3
+//		Imshow.imshow(bj);
+		bj = ed.close(bj, 3); // rayon de n-pixels
+//		Imshow.imshow(bj);
+		BufferedImage bjMedian = mf.median(bj);
+//		Imshow.imshow(bjMedian);
 		
 		//test 1.1 : sobel > border > median > fusion
 		BufferedImage imgSobel = sb.sobel(img); // application de sobel sur l'image original
-		//augmente la taille pour appliquer le filtre median 5x5
-		bj = mf.createBlackBorder(bj); //ajuste la taille de l'image
-		//Imshow.imshow(bj);
-		BufferedImage imgSobel5 = mf.median5(imgSobel); //suppression de bruit sur sobel lvl.5
-		BufferedImage imgFinal = ic.fusionImgEtSobel(bj, imgSobel5, seuil); //applique les contours de sobel sur l'image bruite
-
-
-		Imshow.imshow(imgFinal);
-		imgFinal=mf.median(imgFinal); //supression de bruit
-		imgFinal=mf.median(imgFinal);
-		imgFinal=mf.median(imgFinal);
-		//avec plusieurs application mediant, meilleur resultat
-		Imshow.imshow(imgFinal);
+//		Imshow.imshow(imgSobel);
+		imgSobel = mf.median(imgSobel);
+//		Imshow.imshow(imgSobel);
+		float seuilb = seuil;
 		
-		//on realise une ouverture
-		//dilatation 
+		
+		seuilb -= (seuil*0.65);
+		System.out.println("seuilb vaut "+seuilb+".");
+		
+		imgSobel = ic.seuillage(imgSobel, seuilb);
+//		Imshow.imshow(imgSobel);
+		BufferedImage imgFinal = ic.fusionImgEtSobel(bjMedian, imgSobel, (int)seuil-20); //applique les contours de sobel sur l'image bruite
+		imgFinal = mf.median(imgFinal);
+
 //		Imshow.imshow(imgFinal);
-		imgFinal= ed.erode(imgFinal, 3);
-		//erode
-//		imgFinal=ed.erode(imgFinal, 3);
-		
-		imgFinal = mf.median(imgFinal);		imgFinal = mf.median(imgFinal);		imgFinal = mf.median(imgFinal);
-
-		
-
 
 		/**
-		 * TODO generique [ok] : test qui fonctionne ne pas toucher, 
+		 * Generique [ok] : test qui fonctionne ne pas toucher, 
 		 * connexite pour compter le nombre d'objet (marche) noir sur l'image
 		 * 
 		 * affichage du resultat
 		 */
-		imgFinal = ic.inverseBinary(imgFinal);
+		imgFinal = ic.inverseBinary(imgFinal); //change les marches "Noir" en "Blanc"
+		Imshow.imshow(imgFinal);
 		BufferedImage cc = Label8.getCC(imgFinal);
 		Imshow.imshow(cc);
 		int nbColor = Label8.getNumberOfCC(cc);
-		System.out.println("nombre de marche avec label8 : "+nbColor);
+		System.out.println("nombre de marche avec label8 : "+nbColor); //-1 car on ne va pas prendre en compte la couleur noire
 	}
 }
